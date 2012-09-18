da08c7c1ecd37ac3ac994bdbda267b45665c420f
Arcath <adam@arcath.net>
2012-09-18 19:46:21 +0100

    actually fix #1

--- app/assets/javascripts/layout.js
+++ app/assets/javascripts/layout.js
@@ -10,8 +10,18 @@ $(function(){
 		$.getScript("/users/" + $('#profile_link').attr("data-user-id") + "/set_world/" + $(this).attr("data-world-id"))
 		return false;
 	});
+	
+	check_window_height();
 });
 
 function loading(box){
 	box.html("Loading...")
+}
+
+function check_window_height(){
+	wh = $(window).height();
+	dh = $('#main').height();
+	if((dh + 60) < wh){
+		$('.tgw-footer').css("margin-top", (wh - (dh + 60)))
+	}
 }
